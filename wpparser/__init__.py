a49d117d3c1890003f517c4a8e9fc445efda1169
Martin Sandström <martin@marteinn.se>
2015-07-29 14:54:21 +0200

    Bumped version

--- wpparser/__init__.py
+++ wpparser/__init__.py
@@ -7,18 +7,16 @@ wpparser
 wpparser is a library that parses the wordpress export file into a easy
 readable dictionary.
 
-:copyright: (c) 2014 by Martin Sandström
+:copyright: (c) 2014-2015 by Martin Sandström
 :license: MIT, see LICENSE for more details.
 """
 
 __title__ = "wpparser"
-__version__ = "1.0.3"
-__build__ = 103
+__version__ = "1.0.4"
+__build__ = 104
 __author__ = "Martin Sandström"
 __license__ = "MIT"
-__copyright__ = "Copyright 2014 Martin Sandström"
+__copyright__ = "Copyright 2014-2015 Martin Sandström"
 
 
-from .parser import (
-    parse
-)
+from .parser import parse  # NOQA
