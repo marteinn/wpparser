2bbfcaffb68ca779f2930d70f2996201ce80e88b
Martin Sandström <martin@marteinn.se>
2015-07-29 17:51:22 +0200

    Bumped version

--- wpparser/__init__.py
+++ wpparser/__init__.py
@@ -12,8 +12,8 @@ readable dictionary.
 """
 
 __title__ = "wpparser"
-__version__ = "1.0.5"
-__build__ = 105
+__version__ = "1.0.6"
+__build__ = 106
 __author__ = "Martin Sandström"
 __license__ = "MIT"
 __copyright__ = "Copyright 2014-2015 Martin Sandström"
