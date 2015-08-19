ee2b1c850a649f5284130305f36f76729d2590d0
Martin Sandström <martin@marteinn.se>
2015-08-19 20:13:10 +0200

    Fixed pip install issue. Added pypy to travis

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ import sys
 import pip
 from pip.req import parse_requirements
 from setuptools import setup
-from pypandoc import convert
 
 import wpparser
 
@@ -29,7 +28,11 @@ requires = parse_requirements("requirements/tests.txt",
 tests_require = [str(ir.req) for ir in requires]
 
 # Convert markdown to rst
-long_description = convert('README.md', 'rst')
+try:
+    from pypandoc import convert
+    long_description = convert("README.md", "rst")
+except:
+    long_description = ""
 
 setup(
     name="wpparser",
