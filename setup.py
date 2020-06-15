67ebbf5405f4f3784fdc2f1369302204b88f45bb
Ross Miller <github@decodedcollective.com>
2020-06-15 13:44:21 +0100

    Make parse_requirements compatible with newer pip
    On newer versions on of pip, install is failing with ```ModuleNotFoundError: No module named 'pip.req'```. This fixes that issue.

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@
 import os
 import sys
 import pip
-from pip.req import parse_requirements
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
 from setuptools import setup
 
 import wpparser
